b387b3e4fbea2e405d6ee17938f651b3b33ad3b2
Ritesh Mendhekar <72223003+RiteshM09@users.noreply.github.com>
2026-08-30 11:52:10 +0530

    add project file 04 event coundown timer

---
+++ 04-Countdown-Timer/script.js
@@ -0,0 +1,92 @@
+// Name input element 
+const formName = document.getElementById("name");
+// tag to diplay event
+const display = document.getElementById("displayEvent");
+
+// date
+// get date from form
+const eventDate = document.getElementById("futureDate");
+//  Get current date directly in "YYYY-MM-DD" format
+const currentDate = new Date().toISOString().split('T')[0];
+
+
+document.getElementById("outer").style.display = "none";// hide the output at start 
+//form 
+const formMain = document.getElementById("form");
+//countdown
+const countdownDisplay = document.getElementById("countdown"); 
+
+// form content
+const formContent = document.getElementById("form");
+
+let timerInterval; // to manage timer loop (resubmition,hadling -ve date)
+function FormSubmission(e){
+    e.preventDefault();// prevent frm reloading
+
+    // clear previou msg and countdown
+     clearInterval(timerInterval);
+
+    //get EVENT time(in ms) from event date
+    const eventDateInMs = new Date(eventDate.value).getTime(); 
+    console.log(eventDateInMs);
+    
+    
+    timerInterval = setInterval(function() {
+
+           // get current date(in ms)
+        const currentDateInMS = Date.now();
+
+        // calculate conversion frm ms to ( day | hr | min | sec )
+        let timediff = eventDateInMs - currentDateInMS;
+        console.log(timediff);
+        //  ms -> sec -> min -> hr -> days
+        const day = Math.floor(timediff/(1000*60*60*24));
+        timediff = timediff%((1000*60*60*24));// Remaining TIME in hr (in ms)
+        
+        const hr = Math.floor(timediff/(1000*60*60));
+        timediff = timediff%((1000*60*60)); 
+
+        const min = Math.floor(timediff/(1000*60));
+        timediff = timediff%(1000*60);
+
+        const sec = Math.floor(timediff/(1000));
+
+        console.log( Math.floor((timediff)%1000*60*60*24));
+
+        // console.log(currentDate);
+        // console.log(eventDate.value);
+        
+        // PREVENT NEGATIVE COUNTDOWNS
+        if (timediff <= 0) {
+            display.textContent = formName.value;
+            clearInterval(timerInterval); 
+        }
+
+        // card style 
+        // after submition handle font and outer content
+        document.getElementById("simpleInfo").style.display = "none";// hide the form content 
+        document.getElementById("outer").style.display = "block";// display result
+
+
+
+        if(eventDate.value>currentDate)
+        {   // hide form content after submittion
+            formContent.style.display = "none";
+            
+            // console.log(formName.value);
+            display.textContent = formName.value;
+
+            // console.log(eventDate.value);
+            countdownDisplay.textContent = day + "days " + hr + "hr " + min + "min " + sec + "s remaining";
+        }
+        else{
+            display.textContent = "Invalid Date(accepts future date only)";
+            countdownDisplay.textContent = "";
+        }
+
+    },1000);
+   
+}
+// 
+formMain.addEventListener('submit',FormSubmission);
+
